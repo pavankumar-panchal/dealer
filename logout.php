3fac65f9f94de8f3ececdde436f4f999976f9b37
Pavankumar <84263246+pavankumar-panchal@users.noreply.github.com>
2023-11-30 10:34:31 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?
+	include('./functions/phpfunctions.php');
+	if(imaxgetcookie('dealeruserid') <> '' || imaxgetcookie('dealeruserid') <> false )
+	{
+		$userid = imaxgetcookie('dealeruserid'); 
+		$eventquery = "Insert into inv_logs_event(userid,system,eventtype,eventdatetime) values('".$userid."','".$_SERVER['REMOTE_ADDR']."','149','".date('Y-m-d').' '.date('H:i:s')."')";
+		$eventresult = runmysqlquery($eventquery);
+	}
+	imaxdealerlogout();
+	header('Location:./index.php');
+	
+?>
