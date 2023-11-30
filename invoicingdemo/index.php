0094ad7b28efdd0dc44c10e44ea3a48c0d6db7eb
Pavankumar <84263246+pavankumar-panchal@users.noreply.github.com>
2023-11-30 12:58:42 +0530

    Add files via upload

---
+++ invoicingdemo/index.php
@@ -0,0 +1,223 @@
+<? 
+include('../functions/phpfunctions.php');
+//include('../inc/session.php'); 
+/*session_start();
+if(imaxgetcookie('userid') == false) { $url = '../index.php'; header("Location:".$url); }
+else
+$userid = imaxgetcookie('userid');*/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Invoicing Demo Videos</title>
+</head>
+<link href="../style/main.css?dummy=<? echo (rand());?>" rel=stylesheet>
+<script language="javascript" src="../functions/javascript.js?dummy=<? echo (rand());?>"></script>
+<script language="javascript" src="../functions/jquery.js?dummy=<? echo (rand());?>"></script>
+<link rel="stylesheet" type="text/css" href="../style/global.css?dummy=<? echo (rand());?>">
+<script src="../Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<body>
+<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#ffffff">
+  <tr>
+    <td valign="top" style="border-bottom:#1f4f66 1px solid;" ><table width="100%" border="0" cellspacing="0" cellpadding="0" id="mainwrap">
+        <tr>
+          <td valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                    <tr>
+                      <td><table width="100%" border="0" cellspacing="0" cellpadding="3">
+                          <tr>
+                            <td align="left" class="header-line">Invoice Demo Videos </td>
+                          </tr>
+                        </table></td>
+                    </tr>
+                    <tr>
+                      <td  style="border-bottom:1px solid #000000" ><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                          <tr >
+                            <td width="49%"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                <tr >
+                                  <td width="19%" valign="top" style="border-right: 1px solid #CCCCCC"><table width="100%" border="0" cellspacing="0" cellpadding="3">
+                                    <tr>
+                                      <td >&nbsp;</td>
+                                    </tr>
+                                    <tr>
+                                      <td >&nbsp;</td>
+                                    </tr>
+                                    <tr></tr>
+                                    <tr>
+                                      <td onclick="tabopen6('1','tabg1')"  id="tabg1h1"><div align="left"><a class="r-text">Simple Invoice</a></div></td>
+                                    </tr>
+                                    <tr>
+                                      <td onclick="tabopen6('2','tabg1')" id="tabg1h2"><div align="left"><a class="r-text">Viewing Existing Invoice and Preview</a></div></td>
+                                    </tr>
+                                    <tr>
+                                      <td onclick="tabopen6('3','tabg1')" id="tabg1h3"><div align="left"><a class="r-text">Pricing in Invoice Generation</a></div></td>
+                                    </tr>
+                                    <tr>
+                                      <td onclick="tabopen6('4','tabg1')" id="tabg1h4"><div align="left"><a class="r-text">Receipts</a></div></td>
+                                    </tr>
+                                    <tr>
+                                      <td onclick="tabopen6('5','tabg1')" id="tabg1h5"><div align="left"><a class="r-text">Invoice Register</a></div></td>
+                                    </tr>
+                                    <tr>
+                                      <td onclick="tabopen6('6','tabg1')" id="tabg1h6"><div align="left"><a class="r-text">Receipt Register</a></div></td>
+                                    </tr>
+                                     <tr>
+                                      <td onclick="tabopen6('7','tabg1')" id="tabg1h7"><div align="left"><a class="r-text">Outstanding Register</a></div></td>
+                                    </tr>
+                                   
+                                  </table></td>
+                                  <td width="81%" valign="top"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                    <tr>
+                                      <td valign="top"><div style="display:block;padding-top:5px;" align="center" id="tabg1c1" >
+                                          <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                            <tr>
+                                              <td><div align="center"><strong>Simple Invoice</strong></div></td>
+                                            </tr>
+                                            <tr>
+                                              <td><div align="center">
+                                                  <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/
+swflash.cab#version=6,0,0,0" width="800" height="528" id="tech" align="middle">
+                                                    <param name="allowScriptAccess" value="sameDomain" />
+                                                    <param name="movie" value="../videofiles/simpleinvoice-dealer.swf" />
+                                                    <param name="quality" value="high" />
+                                                    <embed src="../videofiles/simpleinvoice-dealer.swf" quality="high" width="1000" height="600" name="tech" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                                                  </object>
+                                              </div></td>
+                                            </tr>
+                                          </table>
+                                      </div>
+                                          <div style="display:none;padding-top:5px;" align="center" id="tabg1c2">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                              <tr>
+                                                <td><div align="center"><strong> Viewing Existing Invoice and Preview</strong></div></td>
+                                              </tr>
+                                              <tr>
+                                                <td><div align="center">
+                                                    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/
+swflash.cab#version=6,0,0,0" width="777" height="528" id="tech2" align="middle">
+                                                      <param name="allowScriptAccess" value="sameDomain" />
+                                                      <param name="movie" value="../videofiles/viewinvoice.swf" />
+                                                      <param name="quality" value="high" />
+                                                      <embed src="../videofiles/viewinvoice.swf" quality="high" width="1000" height="600" name="tech" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                                                      </embed>
+                                                    </object>
+                                                </div></td>
+                                              </tr>
+                                            </table>
+                                          </div>
+                                        <div style="display:none;padding-top:5px;" align="center" id="tabg1c3">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                              <tr>
+                                                <td><div align="center"><strong>Pricing in Invoice Generation</strong></div></td>
+                                              </tr>
+                                              <tr>
+                                                <td><div align="center">
+                                                    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/
+swflash.cab#version=6,0,0,0" width="777" height="528" id="tech2" align="middle">
+                                                      <param name="allowScriptAccess" value="sameDomain" />
+                                                      <param name="movie" value="../videofiles/pricingandoffer.swf" />
+                                                      <param name="quality" value="high" />
+                                                      <embed src="../videofiles/pricingandoffer.swf" quality="high" width="1000" height="600" name="tech" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                                                      </embed>
+                                                    </object>
+                                                </div></td>
+                                              </tr>
+                                            </table>
+                                        </div>
+                                        <div style="display:none;padding-top:5px;" align="center" id="tabg1c4">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                              <tr>
+                                                <td><div align="center"><strong> Receipts</strong></div></td>
+                                              </tr>
+                                              <tr>
+                                                <td><div align="center">
+                                                    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/
+swflash.cab#version=6,0,0,0" width="777" height="528" id="tech2" align="middle">
+                                                      <param name="allowScriptAccess" value="sameDomain" />
+                                                      <param name="movie" value="../videofiles/receipts.swf" />
+                                                      <param name="quality" value="high" />
+                                                      <embed src="../videofiles/receipts.swf" quality="high" width="1000" height="600" name="tech" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                                                      </embed>
+                                                    </object>
+                                                </div></td>
+                                              </tr>
+                                            </table>
+                                          </object>
+                                          </div>
+                                        <div style="display:none;padding-top:5px;" align="center" id="tabg1c5">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                              <tr>
+                                                <td><div align="center"><strong> Invoice Register</strong></div></td>
+                                              </tr>
+                                              <tr>
+                                                <td><div align="center">
+                                                    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/
+swflash.cab#version=6,0,0,0" width="777" height="528" id="tech2" align="middle">
+                                                      <param name="allowScriptAccess" value="sameDomain" />
+                                                      <param name="movie" value="../videofiles/invoiceregister.swf" />
+                                                      <param name="quality" value="high" />
+                                                      <embed src="../videofiles/invoiceregister.swf" quality="high" width="1000" height="600" name="tech" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                                                      </embed>
+                                                    </object>
+                                                </div></td>
+                                              </tr>
+                                            </table>
+                                          </object>
+                                          </div>
+                                        <div style="display:none;padding-top:5px;" align="center" id="tabg1c6">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                              <tr>
+                                                <td><div align="center"><strong>  Receipt Register</strong></div></td>
+                                              </tr>
+                                              <tr>
+                                                <td><div align="center">
+                                                    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/
+swflash.cab#version=6,0,0,0" width="777" height="528" id="tech2" align="middle">
+                                                      <param name="allowScriptAccess" value="sameDomain" />
+                                                      <param name="movie" value="../videofiles/receiptregister.swf" />
+                                                      <param name="quality" value="high" />
+                                                      <embed src="../videofiles/receiptregister.swf" quality="high" width="1000" height="600" name="tech" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                                                      </embed>
+                                                    </object>
+                                                </div></td>
+                                              </tr>
+                                            </table>
+                                          </object>
+                                        </div><div style="display:none;padding-top:5px;" align="center" id="tabg1c7">
+                                            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+                                              <tr>
+                                                <td><div align="center"><strong>  Outstanding Register</strong></div></td>
+                                              </tr>
+                                              <tr>
+                                                <td><div align="center">
+                                                    <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/
+swflash.cab#version=6,0,0,0" width="777" height="528" id="tech2" align="middle">
+                                                      <param name="allowScriptAccess" value="sameDomain" />
+                                                      <param name="movie" value="../videofiles/outstandingregister.swf" />
+                                                      <param name="quality" value="high" />
+                                                      <embed src="../videofiles/outstandingregister.swf" quality="high" width="1000" height="600" name="tech" align="middle" allowscriptaccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" /></embed>
+                                                      </embed>
+                                                    </object>
+                                                </div></td>
+                                              </tr>
+                                            </table>
+                                          </object>
+                                        </div></td>
+                                    </tr>
+                                  </table></td>
+                                </tr>
+                              </table></td>
+                          </tr>
+                        </table></td>
+                    </tr>
+                  </table></td>
+              </tr>
+            </table></td>
+        </tr>
+      </table></td>
+  </tr>
+</table>
+</body>
+</html>
